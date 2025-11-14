2302b6dbbec8f9a988f0f395cb1f73d731bbbb55
Kaloyan Staykov <72201682+kylorenkala@users.noreply.github.com>
2025-11-14 16:11:35 +0100

    Delete src/main/java directory

--- src/main/java/RobotStatus.java
+++
@@ -1,7 +0,0 @@
-
-public enum RobotStatus {
-    IDLE,       // Waiting for a task
-    WORKING,    // Actively performing a task
-    CHARGING,   // At a charging station
-    LOW_BATTERY // Idle, but needs to charge
-}
